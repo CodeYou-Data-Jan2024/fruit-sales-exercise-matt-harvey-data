433a27ff56a6e9a96b5470b53523077eba293285
Matt Harvey <jmharv01@louisville.edu>
2024-09-09 13:10:56 -0700

    Executed fruit_sales exercise

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
-# add your code here
+import pandas as pd
+
+df = pd.DataFrame({'': ['2017 Sales', '2018 Sales'], 'Apples': [35, 41], 'Bananas': [21, 34]})
+
+df = df.set_index('')
+
+print(df.head())
+
+df.to_csv('fruit.csv')
